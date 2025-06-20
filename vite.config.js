ee6ca1fe252dc9a2c6eef5e1e7fd9bf38d21f7c6
IslamNabil <91093727+IslamNabiil@users.noreply.github.com>
2025-06-20 19:13:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Noorstudio/", // 👈 اسم الريبو بتاعك
   plugins: [react()],
   server: {
     host: true
   }
 })
+
+
+
+
+
